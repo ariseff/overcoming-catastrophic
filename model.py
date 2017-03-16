afea2d3c9f926d4168cc51d56f1e9a92989d7af0
seffari <ari.seff@gmail.com>
2017-03-15 23:55:54 -0400

    reformat model.py

--- model.py
+++ model.py
@@ -14,104 +14,104 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 class Model:
-	def __init__(self, x, y_):
+    def __init__(self, x, y_):
 
-		in_dim = int(x.get_shape()[1]) # 784 for MNIST
-		out_dim = int(y_.get_shape()[1]) # 10 for MNIST
+        in_dim = int(x.get_shape()[1]) # 784 for MNIST
+        out_dim = int(y_.get_shape()[1]) # 10 for MNIST
 
-		self.x = x # input placeholder
+        self.x = x # input placeholder
 
-		# simple 2-layer network
-		W1 = weight_variable([in_dim,50])
-		b1 = bias_variable([50])
+        # simple 2-layer network
+        W1 = weight_variable([in_dim,50])
+        b1 = bias_variable([50])
 
-		W2 = weight_variable([50,out_dim])
-		b2 = bias_variable([out_dim])
+        W2 = weight_variable([50,out_dim])
+        b2 = bias_variable([out_dim])
 
-		h1 = tf.nn.relu(tf.matmul(x,W1) + b1) # hidden layer
-		self.y = tf.matmul(h1,W2) + b2 # output layer
+        h1 = tf.nn.relu(tf.matmul(x,W1) + b1) # hidden layer
+        self.y = tf.matmul(h1,W2) + b2 # output layer
 
-		self.var_list = [W1, b1, W2, b2]
+        self.var_list = [W1, b1, W2, b2]
 
-		# vanilla single-task loss
-		self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=self.y))
-		self.set_vanilla_loss()
+        # vanilla single-task loss
+        self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=self.y))
+        self.set_vanilla_loss()
 
-		# performance metrics
-		correct_prediction = tf.equal(tf.argmax(self.y,1), tf.argmax(y_,1))
-		self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        # performance metrics
+        correct_prediction = tf.equal(tf.argmax(self.y,1), tf.argmax(y_,1))
+        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-	def compute_fisher(self, imgset, sess, num_samples=200, plot_diffs=False, disp_freq=10):
-		# computer Fisher information for each parameter
+    def compute_fisher(self, imgset, sess, num_samples=200, plot_diffs=False, disp_freq=10):
+        # computer Fisher information for each parameter
 
-		# initialize Fisher information for most recent task
-		self.F_accum = []
-		for v in range(len(self.var_list)):
-			self.F_accum.append(np.zeros(self.var_list[v].get_shape().as_list()))
+        # initialize Fisher information for most recent task
+        self.F_accum = []
+        for v in range(len(self.var_list)):
+            self.F_accum.append(np.zeros(self.var_list[v].get_shape().as_list()))
 
-		# sampling a random class from softmax
-		probs = tf.nn.softmax(self.y)
-		class_ind = tf.to_int32(tf.multinomial(tf.log(probs), 1)[0][0])
+        # sampling a random class from softmax
+        probs = tf.nn.softmax(self.y)
+        class_ind = tf.to_int32(tf.multinomial(tf.log(probs), 1)[0][0])
 
-		if(plot_diffs):
-			# track differences in mean Fisher info
-			F_prev = deepcopy(self.F_accum)
-			mean_diffs = np.zeros(0)
+        if(plot_diffs):
+            # track differences in mean Fisher info
+            F_prev = deepcopy(self.F_accum)
+            mean_diffs = np.zeros(0)
 
-		for i in range(num_samples):
-			# select random input image
-			im_ind = np.random.randint(imgset.shape[0])
-			# compute first-order derivatives
-			ders = sess.run(tf.gradients(tf.log(probs[0,class_ind]), self.var_list), feed_dict={self.x: imgset[im_ind:im_ind+1]})
-			# square the derivatives and add to total
-			for v in range(len(self.F_accum)):
-				self.F_accum[v] += np.square(ders[v])
-			if(plot_diffs):
-				if i % disp_freq == 0 and i > 0:
-					# recording mean diffs of F
-					F_diff = 0
-					for v in range(len(self.F_accum)):
-					    F_diff += np.sum(np.absolute(self.F_accum[v]/(i+1) - F_prev[v]))
-					mean_diff = np.mean(F_diff)
-					mean_diffs = np.append(mean_diffs, mean_diff)
-					for v in range(len(self.F_accum)):
-					    F_prev[v] = self.F_accum[v]/(i+1)
-					plt.plot(range(disp_freq+1, i+2, disp_freq), mean_diffs)
-					plt.xlabel("Number of samples")
-					plt.ylabel("Mean absolute Fisher difference")
-					display.display(plt.gcf())
-					display.clear_output(wait=True)
+        for i in range(num_samples):
+            # select random input image
+            im_ind = np.random.randint(imgset.shape[0])
+            # compute first-order derivatives
+            ders = sess.run(tf.gradients(tf.log(probs[0,class_ind]), self.var_list), feed_dict={self.x: imgset[im_ind:im_ind+1]})
+            # square the derivatives and add to total
+            for v in range(len(self.F_accum)):
+                self.F_accum[v] += np.square(ders[v])
+            if(plot_diffs):
+                if i % disp_freq == 0 and i > 0:
+                    # recording mean diffs of F
+                    F_diff = 0
+                    for v in range(len(self.F_accum)):
+                        F_diff += np.sum(np.absolute(self.F_accum[v]/(i+1) - F_prev[v]))
+                    mean_diff = np.mean(F_diff)
+                    mean_diffs = np.append(mean_diffs, mean_diff)
+                    for v in range(len(self.F_accum)):
+                        F_prev[v] = self.F_accum[v]/(i+1)
+                    plt.plot(range(disp_freq+1, i+2, disp_freq), mean_diffs)
+                    plt.xlabel("Number of samples")
+                    plt.ylabel("Mean absolute Fisher difference")
+                    display.display(plt.gcf())
+                    display.clear_output(wait=True)
 
-		# divide totals by number of samples
-		for v in range(len(self.F_accum)):
-			self.F_accum[v] /= num_samples
+        # divide totals by number of samples
+        for v in range(len(self.F_accum)):
+            self.F_accum[v] /= num_samples
 
-	def star(self):
-		# used for saving optimal weights after most recent task training
-		self.star_vars = []
+    def star(self):
+        # used for saving optimal weights after most recent task training
+        self.star_vars = []
 
-		for v in range(len(self.var_list)):
-			self.star_vars.append(self.var_list[v].eval())
+        for v in range(len(self.var_list)):
+            self.star_vars.append(self.var_list[v].eval())
 
-	def restore(self, sess):
-		# reassign optimal weights for latest task
-		if hasattr(self, "star_vars"):
-			for v in range(len(self.var_list)):
-				sess.run(self.var_list[v].assign(self.star_vars[v]))
+    def restore(self, sess):
+        # reassign optimal weights for latest task
+        if hasattr(self, "star_vars"):
+            for v in range(len(self.var_list)):
+                sess.run(self.var_list[v].assign(self.star_vars[v]))
 
-	def set_vanilla_loss(self):
-		self.train_step = tf.train.GradientDescentOptimizer(0.1).minimize(self.cross_entropy)
+    def set_vanilla_loss(self):
+        self.train_step = tf.train.GradientDescentOptimizer(0.1).minimize(self.cross_entropy)
 
-	def update_ewc_loss(self, lam):
-		# elastic weight consolidation
-		# lam is weighting for previous task(s) constraints
+    def update_ewc_loss(self, lam):
+        # elastic weight consolidation
+        # lam is weighting for previous task(s) constraints
 
-		if not hasattr(self, "ewc_loss"):
-			self.ewc_loss = self.cross_entropy
+        if not hasattr(self, "ewc_loss"):
+            self.ewc_loss = self.cross_entropy
 
-		for v in range(len(self.var_list)):
-			self.ewc_loss += (lam/2) * tf.reduce_sum(tf.multiply(self.F_accum[v].astype(np.float32),tf.square(self.var_list[v] - self.star_vars[v])))
-		self.train_step = tf.train.GradientDescentOptimizer(0.1).minimize(self.ewc_loss)
+        for v in range(len(self.var_list)):
+            self.ewc_loss += (lam/2) * tf.reduce_sum(tf.multiply(self.F_accum[v].astype(np.float32),tf.square(self.var_list[v] - self.star_vars[v])))
+        self.train_step = tf.train.GradientDescentOptimizer(0.1).minimize(self.ewc_loss)
 
 
 
